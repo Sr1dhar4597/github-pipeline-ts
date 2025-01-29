e6a5b9158a66b4d67716d912b9fcb0ec36ee88bf
Noman Nawaz <noman.nawaz@ocloudsolutions.net>
2025-01-29 12:57:05 +0500

    pipeline testing

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import { config } from "dotenv";
+
+const envFile = `.env.${process.env.NODE_ENV || "dev"}`;
+config({ path: envFile }); // Load environment variables from.env file
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ message: "Github pipeline test!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
