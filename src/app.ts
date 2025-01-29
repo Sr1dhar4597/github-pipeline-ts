cf854752162ad91376075a6611d28b96044d4b6d
Noman Nawaz <noman.nawaz@ocloudsolutions.net>
2025-01-29 15:36:43 +0500

    pipeline testing

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ message: "Github pipeline test!" });
+  res.json({ message: "Github pipeline testing!" });
 });
 
 app.listen(PORT, () => {
